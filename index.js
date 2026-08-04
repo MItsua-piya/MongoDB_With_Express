717908ae8b0e94395810bcf5a0eefd60e4e6e800
Mlsua-piya <priyawankhade0314@gmail.com>
2026-08-04 23:12:34 +0530

    database setup complete

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express =require("express");
 const app=express();
 const mongoose =require("mongoose");
 const path =require("path");
+const Chat =require("./models/chat.js");
 //These two lines configure Express to use EJS (Embedded JavaScript) as the template engine and tell Express where your template files are located.
 
 app.set("views",path.join(__dirname,"views"));
-app.set("veiw engine","ejs");
+app.set("view engine","ejs");
 main()
 .then(()=>{
     console.log("connection sucessful");
@@ -17,6 +18,15 @@ main()
 async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");//whatsapp is db name
 }
+let chat1=new Chat({
+    from:"neha",
+    to:"priya",
+    msg:"send me ur notes",
+     created_at:new Date(),
+});
+chat1.save().then((res)=>{
+    console.log(res);
+});
 app.get("/",(req,res)=>{
     res.send("root is working");
 });
---
+++ models/chat.js
@@ -0,0 +1,19 @@
+const mongoose =require("mongoose");
+
+const chatSchema = new mongoose.Schema({
+    from:{
+      type:String,
+    },
+    to:{
+        type:String,
+    },
+    msg:{
+        type:String,
+    },
+    created_at:{
+        type:Date,
+        required:true,
+    },
+})
+const Chat = mongoose.model("Chat",chatSchema);
+module.exports=Chat;
